1a8758f64444961112951103087b1a4f288e3789
bleto68 <bleto68@gmail.com>
2026-01-23 11:32:33 +0100

    Fix: formatADA with ₳ symbol + ALL missing i18n keys (EN+NL)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -13,8 +13,12 @@ export function formatADA(amount: number): string {
 }
 
 export function formatNumber(num: number): string {
-  if (num >= 1000000) return (num / 1000000).toFixed(1) + 'M';
-  if (num >= 1000) return (num / 1000).toFixed(1) + 'K';
+  if (num >= 1000000) {
+    return (num / 1000000).toFixed(1) + 'M';
+  }
+  if (num >= 1000) {
+    return (num / 1000).toFixed(1) + 'K';
+  }
   return num.toString();
 }
 
